7ff31121c41b2e7f1549f32f8df786be0a4fe8bc
aryelh <aryelh@gmail.com>
2023-04-27 21:03:46 -0300

    se implemento crear empleados front y back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+import { RegistrarEmpleadoComponent } from './components/registrar-empleado/registrar-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path :'empleados', component:ListaEmpleadosComponent},
+  { path : '', redirectTo:'empleados',pathMatch:'full'},
+  { path : 'registrar-empleado', component : RegistrarEmpleadoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/empleado.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Empleado } from '../empleado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadoService {
+
+  //Esta URL obtiene el listado de todos los empleados en el backend
+  private baseURL = "http://localhost:8080/api/v1/empleados";
+
+  constructor(private httpClient : HttpClient) { }
+  
+  //este metodo nos devuelve los empleados
+  obtenerListaDeEmpleados():Observable<Empleado[]>{
+    return this.httpClient.get<Empleado[]>(`${this.baseURL}`);
+  }
+
+  //Este metodo nos sirve para registrar un empleado
+  registrarEmpleado(empleado : Empleado) : Observable<Object>{
+    return this.httpClient.post(`${this.baseURL}`, empleado)
+  }
+}
